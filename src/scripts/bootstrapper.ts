63ba7e8aa3230712ba10d1d1cd823caf71169b95
Albert Rodriguez Franco <albertrfranco@gmail.com>
2016-08-09 20:56:38 +0200

    First commit! Welcome GbTS! - Done a very simple (yet complete) scaffolding for the TypeScript project - Included gulp tasks for building

---
+++ src/scripts/bootstrapper.ts
@@ -0,0 +1,13 @@
+﻿class Bootstrapper {
+
+    init() {
+        //let categoriesSelect = (<HTMLSelectElement>document.getElementById('RecipeCategory'));
+        alert("Congrats! I'm up!");
+    }
+
+}
+
+window.onload = () => {
+    var bootstrapper = new Bootstrapper();
+    bootstrapper.init();
+};
